b2cd7fcd04ace9dfc4f8026bd7adeeb4d85cfbc3
tomislav <raptor122436@gmail.com>
2026-03-12 16:00:36 +0100

    metadata, final touches

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import postgres from "postgres";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -110,4 +112,23 @@ export async function deleteInvoice(id: string) {
     // return { message: "Database Error: Failed to Delete Invoice." };
   }
   revalidatePath(`/dashboard/invoices`);
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid Credentials";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
